081312d312cd2062f9d01ebd552ccdfa5a1fa1f1
Ivan <ivanrilopa22@gmail.com>
2026-03-09 08:39:15 +0100

    feat: inicializar proyecto con Spring Boot y configuración de Maven

---
+++ src/main/java/com/amani/amaniapirest/AmaniApirestApplication.java
@@ -0,0 +1,13 @@
+package com.amani.amaniapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaniApirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmaniApirestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amani/amaniapirest/AmaniApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amani.amaniapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmaniApirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
